6d8ef90df366c6d0cd3d6efc80704e2e5160451e
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-07-25 22:49:13 +0300

    test main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"github.com/google/uuid"
+	"log/slog"
+	"os"
+	"time"
+
 	"github.com/trust-me-im-an-engineer/demo-subscription-agregator/internal/config"
+	"github.com/trust-me-im-an-engineer/demo-subscription-agregator/internal/models"
+	"github.com/trust-me-im-an-engineer/demo-subscription-agregator/internal/repository/postgres"
 	"github.com/trust-me-im-an-engineer/demo-subscription-agregator/pkg/logger"
 )
 
 func main() {
 	logger.Init()
 
-	_ = config.Load()
+	cfg, err := config.Load()
+	if err != nil {
+		slog.Error("failed to load config: ", err)
+	}
+
+	db, err := postgres.New(context.Background(), cfg.DB)
+	if err != nil {
+		slog.Error("failed to connect to database")
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	id, err := db.CreateSubscription(context.Background(), models.Subscription{
+		ServiceName: "test service",
+		Price:       123,
+		UserID:      uuid.UUID{},
+		StartDate:   time.Now(),
+		EndDate:     sql.NullTime{},
+	})
+	if err != nil {
+		slog.Error("failed to create subscription", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("subscription created", "id", id.String())
 }
--- go.mod
+++ go.mod
@@ -7,14 +7,17 @@ require (
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.8.1
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect

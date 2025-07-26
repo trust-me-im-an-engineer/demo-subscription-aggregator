ca7d45452877338959413a53f57d611c4d7d2c1d
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-07-26 17:41:26 +0300

    service interface

---
+++ internal/service/service.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"context"
+	"github.com/google/uuid"
+
+	"github.com/trust-me-im-an-engineer/demo-subscription-agregator/internal/models"
+)
+
+type ErrInvalidDateRange struct{}
+
+func (e *ErrInvalidDateRange) Error() string {
+	return "end date cannot be before start date"
+}
+
+type SubscriptionService interface {
+	CreateSubscription(ctx context.Context, req models.CreateSubscriptionRequest) (models.SubscriptionResponse, error)
+	GetSubscriptionByID(ctx context.Context, id uuid.UUID) (models.SubscriptionResponse, error)
+	GetAllSubscriptions(ctx context.Context) ([]models.SubscriptionResponse, error)
+	UpdateSubscription(ctx context.Context, id uuid.UUID, req models.UpdateSubscriptionRequest) (models.SubscriptionResponse, error)
+	DeleteSubscription(ctx context.Context, id uuid.UUID) error
+	GetTotalCost(ctx context.Context, filter models.TotalCostRequest) (models.TotalCostResponse, error)
+}

4eddcc4bb14b3b86e23bae0c4884249bd9a78477
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-08-11 21:54:35 +0300

    add indexes

---
+++ migrations/002.init.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS idx_subscriptions_service_name;
+DROP INDEX IF EXISTS idx_subscriptions_user_id;
---
+++ migrations/002.init.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS idx_subscriptions_service_name ON subscriptions(service_name);
+CREATE INDEX IF NOT EXISTS idx_subscriptions_user_id ON subscriptions(user_id);
